2f2b136daa08099d5ab3c812a146699d9f5199b5
zhourui <zhourui@tuhu.cn>
2020-12-26 15:00:08 +0800

    添加eureka-client

--- web/src/main/java/com/web/WebApplication.java
+++ web/src/main/java/com/web/WebApplication.java
@@ -3,7 +3,9 @@ package com.web;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @MapperScan("com.web.*.mapper")
 @SpringBootApplication
 public class WebApplication {
